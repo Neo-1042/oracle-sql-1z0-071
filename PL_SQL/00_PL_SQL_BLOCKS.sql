924c18993d422f744d499c63d83b68ea5810109a
Neo-1042 <rafaelh.71642@gmail.com>
2026-01-22 10:49:02 -0600

    SELECT ... INTO v_first_name, v_last_name

--- PL_SQL/00_PL_SQL_BLOCKS.sql
+++ PL_SQL/00_PL_SQL_BLOCKS.sql
@@ -7,7 +7,7 @@ SET SERVEROUTPUT ON; -- Habilitar OUTPUT
 -- ANONYMOUS PL/SQL Block:
 DECLARE
 -- Declaración de variables
-    v_mi_texto VARCHAR2(20)
+    v_mi_texto VARCHAR2(20);
 BEGIN
     v_mi_texto := '¡Hola, mundo!';
     DBMS_OUTPUT.PUT_LINE(v_mi_texto);
@@ -35,5 +35,23 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Nombre: ' || nombre);
     DBMS_OUTPUT.PUT_LINE('Edad: ' || edad);
     DBMS_OUTPUT.PUT_LINE('Fecha de nacimiento: ' || f_nacimiento);
+END;
+---------------------------------------------------------------------------------------------------
+-- SELECT INTO
+-- Si dejamos un SELECT * FROM tbl WHERE condition; solo, habrá un error, porque se debe hacer
+-- algo con el (los) resultado(s) obtenido(s).
+---------------------------------------------------------------------------------------------------
+DECLARE
+    v_first_name VARCHAR2(15);
+    v_last_name VARCHAR2(15);
+    v_salary employees.salary%TYPE; -- Anchored type
+BEGIN
+    -- El número de variables debe coincidir con los resultados obtenidos
+    SELECT e.first_name, e.last_name 
+        INTO v_first_name, v_last_name
+    FROM employees e
+    WHERE e.employee_id = 100;
+
+    DBMS_OUTPUT.PUT_LINE('Nombre del empleado con id 100 = ' || v_first_name || ' ' || v_last_name);
 END;
 ---------------------------------------------------------------------------------------------------
