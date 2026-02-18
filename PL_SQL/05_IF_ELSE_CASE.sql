eb47372cb721b3be0b0557b4ba399ffc074b9e4d
Neo-1042 <rafaelh.71642@gmail.com>
2026-02-17 20:37:23 -0600

    IF, ELSIF, ELSE, CASE

---
+++ PL_SQL/05_IF_ELSE_CASE.sql
@@ -0,0 +1,60 @@
+---------------------------------------------------------------------------------------------------
+-- IF, END IF, ELSE
+---------------------------------------------------------------------------------------------------
+SET SERVEROUTPUT ON;
+DECLARE
+    v_edad NUMBER;
+BEGIN 
+    v_edad := 25;
+
+    -- Los paréntesis son opcionales
+    IF (v_edad >= 18) THEN
+        DBMS_OUTPUT.PUT_LINE('Eres mayor de edad');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Eres menor de edad');
+    END IF;
+END;
+/
+---------------------------------------------------------------------------------------------------
+DECLARE
+    v_salary NUMBER;
+BEGIN
+    SELECT e.salary INTO v_salary
+    FROM tbl_employees e
+    WHERE e.employee_id = 200;
+
+    IF (v_salary > 1000 AND v_salary <= 5000) THEN
+        DBMS_OUTPUT.PUT_LINE('Empleado de categoría C');
+    ELSIF (v_salary > 5000 AND v_salary <= 10000) THEN
+        DBMS_OUTPUT.PUT_LINE('Empleado de categoría B');
+    ELSIF (v_salary > 10000) THEN
+        DBMS_OUTPUT.PUT_LINE('Empleado de categoría A');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Salario no valido');
+    END IF;
+
+    -- BETWEEN (Intervalo cerrado, es decir, incluye los extremos.
+END;
+/
+---------------------------------------------------------------------------------------------------
+-- CASE
+---------------------------------------------------------------------------------------------------
+DECLARE
+    v_departamento tbl_department.department_name%TYPE;
+BEGIN
+    SELECT d.department_name
+        INTO v_departamento
+    FROM tbl_department d
+    WHERE d.department_id = 200;
+
+    CASE v_departamento
+        WHEN 'Operations' THEN 
+            DBMS_OUTPUT.PUT_LINE('El departamento se encuentra en el piso 4');
+        WHEN 'IT Support' THEN
+            DBMS_OUTPUT.PUT_LINE('El departamento se encuentra en el piso 3');
+        WHEN 'NOC' THEN
+            DBMS_OUTPUT.PUT_LINE('El departamento se encuentra en el piso 3');
+    ELSE 'Departamento no valido'
+    END CASE;
+END;
+/
